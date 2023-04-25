acf29cb4ebf1c6d35654a66c7811c60a2f0a642b
AntonIvasheka <anton.ivasheka@gmail.com>
2023-04-25 22:59:59 +0300

    year

---
+++ src/home_work_1/Task46.java
@@ -0,0 +1,25 @@
+package home_work_1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Task46 {
+    public static void main(String[] args) {
+        Scanner in;
+        int year;
+        while (true) {
+            in = new Scanner(System.in);
+            try {
+                System.out.println("Определение года на високосность. Введите год : ");
+                year = in.nextInt();
+                break;
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Wrong value, repeat!");
+            }
+        }
+        if (year%4 != 0) System.out.println("Обычный год");
+            else if (year%100 ==0 && year%400 == 0) System.out.println("Обычный год");
+                else System.out.println("Високосный год");
+    }
+}
