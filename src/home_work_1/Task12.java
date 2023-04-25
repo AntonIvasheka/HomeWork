0b0d322511369886243ba13694c02ec346dbcc09
AntonIvasheka <anton.ivasheka@gmail.com>
2023-04-25 18:33:40 +0300

    not worrking (commit inside)

---
+++ src/home_work_1/Task12.java
@@ -0,0 +1,19 @@
+/*import java.util.Scanner;
+
+public class Task12 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Первое число: 42.5");
+        double firstNumber = 42.5;
+        System.out.println("Введите второе число: ");
+        double secondNumber = in.nextDouble();
+        double a = firstNumber & secondNumber;
+        double b = firstNumber | secondNumber;
+        System.out.println("Результат побитовой операции И: " + a);
+        System.out.println("Результат побитовой операции ИЛИ: " + b);
+   }
+}
+ bad operand types for binary operator '&'
+ Поразрядные операции выполняются над отдельными разрядами или битами чисел.
+ В данных операциях в качестве операндов могут выступать только целые числа.
+ https://metanit.com/java/tutorial/2.13.php */
