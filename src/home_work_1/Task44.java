07c05e462520fdb7289cff68702de5f4a8ce2eea
AntonIvasheka <anton.ivasheka@gmail.com>
2023-04-25 20:56:26 +0300

    B/kB

---
+++ src/home_work_1/Task44.java
@@ -0,0 +1,32 @@
+package home_work_1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Task44 {
+    public static void main(String[] args) {
+        int choice;
+        Scanner in;
+        double number;
+        while (true) {
+            in = new Scanner(System.in);
+            try {
+                System.out.println("Какое действиие будем делать(байты в килобайты - 1; килобайты в байты - 2): ");
+                choice = in.nextInt();
+                System.out.println("Численное значение (б/кб): ");
+                number = in.nextDouble();
+                break;
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Wrong value, repeat!");
+            }
+        }   // Можно проверку на значение выбора запилить и ввод байтов ограничить вещественным числом,
+            // при умножении которого на 8 (т.е. переводом в биты) получалось число без вещественной части.
+        if (choice == 1)
+            System.out.println("Результат перевода в кб : " + number/1024);
+            else if (choice == 2)
+                System.out.println("Результат перевода в байты : " + number*1024);
+                else System.out.println("Не верный выбор. Повторите программу");
+
+    }
+}
