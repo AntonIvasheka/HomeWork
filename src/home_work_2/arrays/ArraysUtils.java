e7d4793c4092ef88ed4405a234ff219242c1d4fc
AntonIvasheka <anton.ivasheka@gmail.com>
2023-05-04 13:27:52 +0300

    + ArraysUtils.java

--- src/home_work_2/arrays/ArraysUtils.java
+++ src/home_work_2/arrays/ArraysUtils.java
@@ -1,4 +1,28 @@
 package home_work_2.arrays;
 
+import java.util.Random;
+import java.util.Scanner;
+
+import static java.lang.Math.random;
+
 public class ArraysUtils {
+    public static int[] arrayFromConsole() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите размер массива:");
+        int arrayVal = in.nextInt();
+        int[] arrayFromConsole = new int[arrayVal];
+        System.out.println("Введите элементы массива:");
+            for (int i = 0; i < arrayVal; i++) {
+                arrayFromConsole[i] = in.nextInt();
+                }
+        return arrayFromConsole;
+    }
+    public static int[] arrayRandom(int size, int maxValueExclusion) {
+        int[] arrayRandom = new int[size];
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            arrayRandom[i] = random.nextInt(maxValueExclusion);
+        }
+        return arrayRandom;
+    }
 }
