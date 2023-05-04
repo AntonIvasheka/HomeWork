3e416ac6d4cfd96ec7105c1e67ba1fc261d3b2f1
AntonIvasheka <anton.ivasheka@gmail.com>
2023-05-04 13:33:07 +0300

    + ArraysIteration

---
+++ src/home_work_2/arrays/ArraysIteration.java
@@ -0,0 +1,41 @@
+package home_work_2.arrays;
+
+import static home_work_2.arrays.ArraysUtils.arrayFromConsole;
+
+public class ArraysIteration {
+    public static void main(String[] args) {
+        int[] prArray = arrayFromConsole();
+        array221(prArray);
+        array222(prArray);
+        array223(prArray);
+    }
+
+    public static void array221(int[] prArray) {
+        for (int k : prArray) {
+            System.out.print(k + " ");
+        }
+        System.out.println();
+    }
+    public static void array222(int[] prArray) {
+        int arrayLength = prArray.length;
+        int i = 0;
+        while (i != arrayLength) {
+            if (i%2 != 0) {
+                System.out.print(prArray[i] + " ");
+            }
+            i++;
+        }
+        System.out.println();
+    }
+    public static void array223(int[] prArray) {
+        int arrayLength = prArray.length;
+        int j = 0;
+        do {
+            arrayLength = arrayLength -1;
+            System.out.print(prArray[arrayLength] + " ");
+        } while (arrayLength > 0);
+        System.out.println();
+    }
+
+
+}
