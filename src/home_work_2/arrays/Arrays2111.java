9f55a0b4901b8900a0fa17307a2cb0537c9eaa11
AntonIvasheka <anton.ivasheka@gmail.com>
2023-05-04 13:29:46 +0300

    + Arrays2111.java

---
+++ src/home_work_2/arrays/Arrays2111.java
@@ -0,0 +1,12 @@
+package home_work_2.arrays;
+
+import static home_work_2.arrays.ArraysUtils.arrayFromConsole;
+
+public class Arrays2111 {
+    public static void main(String[] args) {
+        int[] conteiner = arrayFromConsole();
+        for (int i : conteiner) {
+            System.out.print(i + " ");
+        }
+    }
+}
