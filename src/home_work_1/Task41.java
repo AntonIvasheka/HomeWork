9067076b0177e48e1b3d0903b90c196310674de5
AntonIvasheka <anton.ivasheka@gmail.com>
2023-05-24 16:43:05 +0300

    + удалена точка входа, отредактировано возвращаемое значение

---
+++ src/home_work_1/Task41.java
@@ -0,0 +1,14 @@
+package home_work_1;
+
+public class Task41 {
+    public static boolean calc(double b) {
+        if (b % 2 == 0) {
+            System.out.println("Введенное число: четное");
+            return true;
+        }
+        else {
+            System.out.println("Введённое число: нечетное");
+            return false;
+        }
+    }
+}
