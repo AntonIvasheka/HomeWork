506fe660c400afa74c7c22c18f6b3f0879e61942
AntonIvasheka <anton.ivasheka@gmail.com>
2023-05-25 13:56:38 +0300

    + добавил тест с 0!

--- src/tests/home_work_2/loops/Loops112test.java
+++ src/tests/home_work_2/loops/Loops112test.java
@@ -1,31 +1,32 @@
-/*package tests.home_work_2;
+package tests.home_work_2.loops;
 
-import home_work_2.loops.Loops111;
 import home_work_2.loops.Loops112;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class Test112test {
-    @Test
+public class Loops112test {
+        @Test
         public void factorial() {
             long number = 5;
-            String str = "";
             String expected = "1 * 2 * 3 * 4 * 5 = 120";
-            Assertions.assertEquals(expected, Loops112.factorial(number));
+            Assertions.assertEquals(expected, Loops112.textFact(number));
         }
         @Test
         public void printFact2() {
             long number = 3;
-            String str = "";
             String expected = "1 * 2 * 3 = 6";
-            Assertions.assertEquals(expected, Loops112.factorial(number));
+            Assertions.assertEquals(expected, Loops112.textFact(number));
         }
         @Test
         public void printFact3() {
             long number = 7;
-            String str = "";
             String expected = "1 * 2 * 3 * 4 * 5 * 6 * 7 = 5040";
-            Assertions.assertEquals(expected, Loops112.factorial(number));
+            Assertions.assertEquals(expected, Loops112.textFact(number));
         }
-    }
-*/
+        @Test
+        public void printFact4() {
+                long number = 0;
+                String expected = "0! = 1";
+                Assertions.assertEquals(expected, Loops112.textFact(number));
+        }
+}
