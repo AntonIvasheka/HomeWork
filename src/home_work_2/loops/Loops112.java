c967ebe0ece85298e961750debe93923c7a317d7
AntonIvasheka <anton.ivasheka@gmail.com>
2023-05-25 13:55:56 +0300

    + подправил тестируемый класс

--- src/home_work_2/loops/Loops112.java
+++ src/home_work_2/loops/Loops112.java
@@ -1,22 +1,32 @@
 package home_work_2.loops;
 
 public class Loops112 {
-    public static void main(String[] args) {
-        long n = 10;
-        long k = n+1;
-        System.out.println(factorial(n, k));
+    public static String textFact(long n) {
+        String resStr="";
+        if (n != 0) {
+            for (int i = 1; i <= n; i++) {
+                if (i < n) {
+                    System.out.print(i + " * ");
+                    resStr = resStr + i + " * ";
+                } else {
+                    System.out.print(i + " = " + factorial(n));
+                    resStr = resStr + i + " = " + factorial(n);
+                }
+            }
+        } else if (n == 0) {
+            System.out.println("0! = " + factorial(n));
+            resStr = resStr + "0! = " + factorial(n);
+        } else {
+            System.out.println("Введено не верное число");
+        }
+        System.out.println();
+        return resStr;
     }
-    public static long factorial(long n, long k) {
+    public static long factorial(long n) {
         if (n == 0) {
             return 1;
         }else {
-            System.out.print(k - n);
-            if (n != 1) {
-                System.out.print(" * ");
-            } else {
-                System.out.print(" = ");
-            }
-            return n * factorial(n - 1, k);
+            return n * factorial(n - 1);
         }
     }
 }
