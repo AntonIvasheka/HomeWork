0b62f1f83ed7a17ccaae7f144a3b749a82379ebe
AntonIvasheka <anton.ivasheka@gmail.com>
2023-06-24 01:13:04 +0300

    п.7 доделал, но кодировки у txt файлов кривые(на первом попробовал)...

--- src/home_work_6/BookList.java
+++ src/home_work_6/BookList.java
@@ -1,6 +1,6 @@
 package home_work_6;
 
-import java.io.File;
+import java.io.*;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -13,6 +13,8 @@ public class BookList {
         Map <Integer, String> bookMap = new LinkedHashMap<>();
         int bookName = 0;
         String word = "";
+        String all = "";
+        String fileName ="";
         Scanner in = new Scanner(System.in);
         try {
             // Создание нового объекта file
@@ -29,11 +31,37 @@ public class BookList {
         for(Map.Entry<Integer, String> entry: bookMap.entrySet()) {
             System.out.println(entry.getKey() + " - " + entry.getValue());
         }
-        System.out.println("Введите номер книги : ");
-        bookName = in.nextInt();
-        System.out.println("Введите что найти : ");
-        word = in.next();
+        do {
+            System.out.println("Введите номер книги (0 - выход) : ");
+            bookName = in.nextInt();
+            if (bookName == 0) {
+                break;
+            }
+            System.out.println("Введите что найти : ");
+            word = in.next();
+            fileName = "HomeWork/src/home_work_6/books/" + bookMap.get(bookName);
+            try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+                //чтение построчно
+                String s;
+                while ((s = br.readLine()) != null) {
+                    //System.out.println(s);
+                    all = all + s;
+                }
+            } catch (
+                    IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+            EasySearch searchWord = new EasySearch();
+            System.out.println(searchWord.search(all, word));
 
+            String toWrite = "HomeWork/src/home_work_6/result.txt";
+            File file = new File(toWrite);
+            try (Writer data = new FileWriter(file, true)) {
+                data.write(bookMap.get(bookName) + " - " + word + " - " + (searchWord.search(all, word)) + "\n");
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (true);
     }
 
 }
